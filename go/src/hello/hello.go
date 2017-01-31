6a43d64cda983ea651bda6ae1859049c7c2e48be
Derek Moore <derek@ci.ops.ripple.com>
2017-01-31 02:11:04 +0000

    Add example of passing strings to Go functions

--- go/src/hello/hello.go
+++ go/src/hello/hello.go
@@ -1,5 +1,11 @@
 package hello
 
+import "fmt"
+
 func HelloFromGo() string {
-    return "Hello from Go!"
+    return "HelloFromGo(): Hello from Go!"
+}
+
+func PrintFromGo(str string) {
+    fmt.Printf("PrintFromGo(): %s\n", str)
 }
--- node/demo.js
+++ node/demo.js
@@ -1,18 +1,23 @@
 var ffi = require('ffi');
+var ref = require('ref');
 var finalize = require('finalize');
 
 // Load library and function prototypes
 var lib = ffi.Library('./libhello.so', {
     'HelloFromGo': [ 'char *', [] ],
+    'PrintFromGo': [ 'void', [ 'char *' ] ],
     'FreeCString': [ 'void', [ 'void *' ] ]
 });
 
-// Call FFI
+// Call library function
 hello = lib.HelloFromGo();
 
-// Attach garbage collector
+// Attach garbage collector to returned string
 finalize(hello, function () {
     lib.FreeCString(this);
 });
 
 console.log(hello.readCString());
+
+// Pass string to library function
+lib.PrintFromGo(ref.allocCString("Calling from Node"));
